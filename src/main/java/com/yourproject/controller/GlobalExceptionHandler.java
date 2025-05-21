911130d2ab14da9f3e3bc9cb21d29021075c3747
nileshrn01 <146316325+r-nilesh-c@users.noreply.github.com>
2025-05-21 18:42:18 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/yourproject/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.yourproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxSizeException(MaxUploadSizeExceededException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("File size exceeds 100MB limit"));
+    }
+}
